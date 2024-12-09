bddf7e062086837935dcd689494ee585281148d5
dirkjan <mailbox@dirkjanbuter.com>
2024-12-09 12:48:10 +0100

    first

---
+++ src/main.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+#define TRUE 1
+#define FALSE 0;
+#define AUDIOMEMEORYLEN 1048576
+
+static int _talk = 0;
+static int _lasttalk = 0;
+static int _audiomemorypos = 0;
+static int _audiomemorymax = 0;
+static float audiomemory[AUDIOMEMEORYLEN];
+
+int filtercreate(int fps) 
+{
+    return TRUE;
+}
+
+int filterdestroy() 
+{
+    return TRUE;
+}
+
+int filterstep(unsigned char *buffer, int w, int h, unsigned int color, char *text, int64_t framecount)
+{
+    return TRUE;
+}
+
+    
+int filteraudio(float *frame, int64_t framecount, float elapsed)
+{
+       if(_talk)
+       {	
+            if(_audiomemorypos > _audiomemorymax - 1024)
+            {
+                 _audiomemorypos = 0;
+            }
+            memcpy(frame, &(audiomemory[_audiomemorypos]), sizeof(float)*1024);
+            _audiomemorypos += 1024;
+       } 
+       return 1;
+}
+
+int filtermic(float *frame, int64_t framecount, float elapsed)
+{
+	float maxf;
+	int i;
+	maxf = 0.0f;
+
+        if(!_talk)
+        {
+            if(_audiomemorypos < AUDIOMEMEORYLEN)
+            {
+                  memcpy(&(audiomemory[_audiomemorypos]), frame, sizeof(float)*1024);
+                  _audiomemorypos += 1024;
+            }
+        }
+	return 1;
+}
+
+int filterkey(int64_t code)
+{
+    _lasttalk = _talk;
+    _talk = !_talk;
+    _audiomemorymax = _audiomemorypos;
+    _audiomemorypos = 0;
+    return 1;
+}
